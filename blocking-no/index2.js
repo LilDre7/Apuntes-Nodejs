b1e3401f923162002d48dfd518ec76165b66f017
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-05-23 20:57:37 -0600

    Solo quiero hacer commits

--- blocking-no/index2.js
+++ blocking-no/index2.js
@@ -4,4 +4,6 @@ const suma = (a, b) => {
 
 console.log((2 + 3));
 
-console.log(" Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sit a quas distinctio, harum iure consequatur reprehenderit provident eaque rerum nobis voluptatem ut quia veritatis aspernatur enim asperiores? Deserunt, voluptates! ");7
+console.log(" Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sit a quas distinctio, harum iure consequatur reprehenderit provident eaque rerum nobis voluptatem ut quia veritatis aspernatur enim asperiores? Deserunt, voluptates! ");
+
+console.log("Hola mundo");
