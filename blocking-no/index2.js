546b44714dec9f96443aa91109dd2389dff632eb
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-05-23 20:57:12 -0600

    cambios

--- blocking-no/index2.js
+++ blocking-no/index2.js
@@ -3,3 +3,5 @@ const suma = (a, b) => {
 }
 
 console.log((2 + 3));
+
+console.log(" Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sit a quas distinctio, harum iure consequatur reprehenderit provident eaque rerum nobis voluptatem ut quia veritatis aspernatur enim asperiores? Deserunt, voluptates! ");7
